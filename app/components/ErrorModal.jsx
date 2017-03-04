94853ee87ba663bd5f13a99963dfb03d7e953359
Oleg Novikov <thefasttracker@gmail.com>
2017-03-04 19:28:39 +0300

    fix ErrorModal bug

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,4 +1,6 @@
  import React, {Component} from 'react'
+ import ReactDOM from 'react-dom'
+ import ReactDOMServer from 'react-dom/server'
 
 export default class ErrorModal extends Component {
 	static defaultProps: {
@@ -11,12 +13,8 @@ export default class ErrorModal extends Component {
 	}
 
 	componentDidMount(){
-		let modal = new  Foundation.Reveal($('#error-modal'))
-		modal.open()
-	} 
-	render() {
 		let {title, message} = this.props
-		return (
+		let modalMarkup = (
 			<div id="error-modal" className="reveal tiny text-center" data-reveal="">
 				<h4>{title}</h4>
 				<p>{message}</p>
@@ -27,6 +25,20 @@ export default class ErrorModal extends Component {
 				</p>
 				
 			</div>
+		)
+
+		let $modal = $(ReactDOMServer.renderToString(modalMarkup))
+		$(ReactDOM.findDOMNode(this)).html($modal)
+		
+		let modal = new Foundation.Reveal($('#error-modal'))
+		modal.open()
+	} 
+	render() {
+		
+		return (
+			<div>
+			</div>
+			
 		)
 	}
 }
